86d9e47a0c0bb99d42e27a4231f63aa066848069
Chad Serrant <chad@chadius.net>
2016-08-27 13:31:47 -0400

    Adding more layers.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ This should demonstrate the various layers involved in the Turn Based Strategy g
 from __future__ import division, print_function, unicode_literals
 
 import pyglet
+from pyglet.graphics.vertexattribute import GLubyte
+
 import cocos
 from cocos.actions import *
 from cocos import tiles, rect
@@ -18,9 +20,17 @@ class ScrollableLayerManager(cocos.layer.scrolling.ScrollingManager, pyglet.even
         self.viewport = rect.Rect(0,0,screen_width, screen_height)
         super(ScrollableLayerManager, self).__init__(self.viewport)
         # Selector Layer
+        self.overlay_layer = OverlayLayer()
+        self.add(self.overlay_layer, 5, 'overlay')
+
         # Shading Layer
+        self.shading_layer = ShadingLayer()
+        self.add(self.shading_layer, 4, 'shading')
+
         # Unit Layer
+
         # Obstacle Layer
+
         # Terrain Layer
         self.terrain_layer = TerrainLayer(screen_width, screen_height)
         self.add(self.terrain_layer, 1, 'terrain')
@@ -93,6 +103,159 @@ class ScrollableLayerManager(cocos.layer.scrolling.ScrollingManager, pyglet.even
         else:
             self.camera_direction_y = 0
 
+class SelectorLayer(cocos.layer.scrolling.ScrollableLayer):
+    # Init: Load the selector sprite
+    # If there are selected tiles, draw them.
+    pass
+
+class OverlayLayer(cocos.layer.scrolling.ScrollableLayer):
+    # This layer is used to highlight terrain.
+    def __init__ (self):
+        super(OverlayLayer, self).__init__()
+
+        # Get the dimensions of the map.
+        map_dimensions = {
+            'width':25,
+            'height':15
+        }
+
+        tile_size = 32
+
+        # Compose the image.
+        composed_image = self.make_highlight(tiles, map_dimensions, tile_size)
+        self.highlighted_terrain_sprite = cocos.sprite.Sprite(composed_image)
+
+        # Add an action to fade the terrain overlay.
+        fade_action = FadeTo(64, 2) + FadeTo(192, 1)
+        self.highlighted_terrain_sprite.do(Repeat(fade_action))
+
+        # Center the overlay on top of the terrain.
+        self.highlighted_terrain_sprite.image_anchor_x = 0
+        self.highlighted_terrain_sprite.image_anchor_y = 0
+        self.highlighted_terrain_sprite.position = (0, 0)
+
+        # Now add the image to this object.
+        self.add(self.highlighted_terrain_sprite)
+
+    def make_highlight(self, tiles, map_dimensions, tile_size):
+        """Return a single image that contains the
+        """
+        # Calculate dimensions of the master image.
+        image_width = map_dimensions['width'] * tile_size;
+        image_height = map_dimensions['height'] * tile_size;
+
+        # Prepare data to store the raw colors.
+        image_data = []
+        tile_colors = {
+            'red' : [128,0,0,255],
+            'blue' : [0,0,128,255],
+            'transparent' : [0,0,0,0],
+        }
+
+        overlay_by_coordinate = {
+            0 : {
+                0 : 'blue',
+                1 : 'blue',
+            },
+            1 : {
+                0 : 'red',
+            },
+            2 : {
+                0 : 'blue',
+            },
+            10 : {
+                0 : 'blue',
+            }
+        }
+
+        for y in range(0, image_height):
+            for x in range(0, image_width):
+                map_coordinate_i = int(x / tile_size)
+                map_coordinate_j = int(y / tile_size)
+
+                # Get the overlay color found at this coordinate.
+                tile_color = 'transparent'
+
+                if map_coordinate_j in overlay_by_coordinate and map_coordinate_i in overlay_by_coordinate[map_coordinate_j]:
+                    tile_color = overlay_by_coordinate[map_coordinate_j][map_coordinate_i]
+                image_data.extend(tile_colors[tile_color])
+
+        # Translate the data to cbytes.
+        rawData = (GLubyte * len(image_data))(*image_data)
+
+        # Create an image containing the image data.
+        highlighted_image = pyglet.image.ImageData(
+            image_width,
+            image_height,
+            'RGBA',
+            rawData
+        )
+
+        return highlighted_image
+
+class ShadingLayer(cocos.layer.scrolling.ScrollableLayer):
+    # This layer is used to shade most of the map.
+    def __init__ (self):
+        super(ShadingLayer, self).__init__()
+
+        # Get the dimensions of the map.
+        map_dimensions = {
+            'width':25,
+            'height':15
+        }
+
+        tile_size = 32
+
+        # Compose the image.
+        composed_image = self.make_shading(map_dimensions, tile_size)
+        self.shading_terrain_sprite = cocos.sprite.Sprite(composed_image)
+
+        # Center the overlay on top of the terrain.
+        self.shading_terrain_sprite.image_anchor_x = 0
+        self.shading_terrain_sprite.image_anchor_y = 0
+        self.shading_terrain_sprite.position = (0, 0)
+
+        # Now add the image to this object.
+        self.add(self.shading_terrain_sprite)
+
+    def make_shading(self, map_dimensions, tile_size):
+        """Return a single image that contains the
+        """
+        # Calculate dimensions of the master image.
+        image_width = map_dimensions['width'] * tile_size;
+        image_height = map_dimensions['height'] * tile_size;
+
+        # Prepare data to store the raw colors.
+        image_data = []
+        shade_color = [0,0,0,127]
+        transparent_color = [0,0,0,0]
+
+        for y in range(0, image_height):
+            for x in range(0, image_width):
+                map_coordinate_i = int(x / tile_size)
+                map_coordinate_j = int(y / tile_size)
+
+                # Determine the shading color based on the coordinate.
+                if map_coordinate_j == 3 and map_coordinate_i == 0:
+                    tile_color = transparent_color
+                else:
+                    tile_color = shade_color
+
+                image_data.extend(tile_color)
+
+        # Translate the data to cbytes.
+        rawData = (GLubyte * len(image_data))(*image_data)
+
+        # Create an image containing the image data.
+        shaded_image = pyglet.image.ImageData(
+            image_width,
+            image_height,
+            'RGBA',
+            rawData
+        )
+
+        return shaded_image
+
 class TerrainLayer(cocos.tiles.RectMapLayer):
     """Builds the surface the units will travel across.
     """
@@ -107,10 +270,9 @@ class TerrainLayer(cocos.tiles.RectMapLayer):
             layer1.cells
         )
 
-        # TODO: Add a 50 pixel buffer.
         # Set the width and height of the layer.
-        self.layer_width = self.px_width + 100
-        self.layer_height = self.px_height + 100
+        self.layer_width = self.px_width
+        self.layer_height = self.px_height
 
 class BackgroundColorLayer(cocos.layer.util_layers.ColorLayer):
     """Draws a static background.
@@ -121,7 +283,7 @@ class BackgroundColorLayer(cocos.layer.util_layers.ColorLayer):
 
 if __name__ == "__main__":
     screen_width = 800
-    screen_height = 300
+    screen_height = 600
 
     cocos.director.director.init(width=screen_width, height=screen_height)
 
